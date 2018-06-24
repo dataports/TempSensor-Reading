461058d7385c61e2b0c3cafa5a96ba217d4a32c6
Administrator <zhangth1990@hotmail.com>
2018-06-24 15:29:43 -0600

    finished reading temperature sensor

--- temperature sensor/ViewController.swift
+++ temperature sensor/ViewController.swift
@@ -7,26 +7,19 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
 
-class ViewController: UIViewController, TemperatureWebServiceDelegate{
+class ViewController: UIViewController{
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        temperatureReceived(temperature: "23.5", date: "Feb 16")
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    override func viewWillAppear(_ animated: Bool)
-    {
-        super.viewWillAppear(animated)
-        
-        var webService = TemperatureWebService()
-        webService.delegate = self
-        webService.startConnection()
-    }
+   
+    let baseURL = "http://192.168.1.169/temperaturejson.php"
     @IBOutlet weak var currentTempLabel: UILabel!
     @IBOutlet weak var lastUpdateLabel: UILabel!
     
@@ -35,6 +28,42 @@ class ViewController: UIViewController, TemperatureWebServiceDelegate{
         currentTempLabel.text = "\(temperature) °C"
         lastUpdateLabel.text = "\(date)"
     }
+    func getTemperatureData(url: String) {
+        
+        Alamofire.request(url, method: .get)
+            .responseJSON { response in
+                if response.result.isSuccess {
+                    
+                    print("Sucess! Got the Temperature data")
+                    let temperatureJSON : JSON = JSON(response.result.value!)
+                    self.updateTemperatureData(json: temperatureJSON)
+                    print(temperatureJSON.count)
+                    
+                    
+                } else {
+                    print("Error: \(String(describing: response.result.error))")
+                    self.currentTempLabel.text = "Connection Issues"
+                    
+                }
+        }
+        
+    }
+    
+    func updateTemperatureData(json : JSON) {
+        var temperatureArrayLength = json.count - 1 
+        if let tempResult = json[temperatureArrayLength]["Temp"].string {
+            currentTempLabel.text =  String(tempResult) + "°C"
+            lastUpdateLabel.text = json[temperatureArrayLength]["Date"].string
+        }else{
+            currentTempLabel.text = "Temperautre Unavailable"
+            lastUpdateLabel.text = json[temperatureArrayLength]["Date"].string
+        }
+    }
+    @IBAction func refreshTemp(_ sender: Any) {
+         getTemperatureData(url: baseURL)
+        
+    }
+    
 
 }
 
